c0d596a0d3ff85e4ee847d11f7f7ebb4000df272
ckidus <whrltkfkd@naver.com>
2023-08-21 00:19:14 +0900

    230820

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function toggleMid_spacingInput() {
+  var Mid_spacingInput = document.getElementById("Mid_spacingInput");
+  var Mid_spacingCheckbox = document.getElementById("Mid_spacingCheckbox");
+  
+  if (Mid_spacingCheckbox.checked) {
+    Mid_spacingInput.style.display = "block";
+  } else {
+    Mid_spacingInput.style.display = "none";
+  }
+}
+
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
@@ -7,6 +18,8 @@ function drawCanvas() {
   var diameter = parseInt(document.getElementById("diameterInput").value);
   var Hori_spacing = parseInt(document.getElementById("Hori_spacingInput").value);
   var Vert_spacing = parseInt(document.getElementById("Vert_spacingInput").value);
+
+  var Mid_spacingVal = parseInt(document.getElementById("Mid_spacingInput").value); //가운데 다를때
 
   context.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
 
@@ -18,6 +31,8 @@ function drawCanvas() {
   var rectBotY = canvas.height-70;
   var rectlenX = xLength;
   var rectlenY = xLength * 2.5;
+
+  var Letterplus = Hori_spacing/2;
   
   var currentX = centerX;
   var currentY = rectBotY - diameter;
@@ -25,16 +40,26 @@ function drawCanvas() {
   var RowCircleCount = xLength * 2.5 / Vert_spacing;
 
   var FirstRowY = rectBotY - diameter;
-  var dataY = rectBotY + diameter;
+  var dataY = rectBotY + 10;
   var circles = [];
 
-  console.log(rectTopX,rectTopY,rectBotX,rectBotY);
+  if (isNaN(Mid_spacingVal)) {
+    Mid_spacingVal = 0; // NaN일 경우 0으로 설정
+  }
 
-  context.beginPath();
-  context.arc(400, 700, 10, 0, 2 * Math.PI); // 점을 그리는 부분
-  context.fillStyle = "blue"; // 점의 색상
-  context.fill();
-  context.closePath();
+  console.log(Mid_spacingVal);
+
+  if (Mid_spacingVal !== "") { // Mid_spacingVal 값이 비어있지 않으면
+    // 여기에 조건문 내용 추가
+    var Mid_spacing = Mid_spacingVal / 2;
+  }
+  console.log(Mid_spacing);
+
+  if(Hori_spacing < 30){
+    Letterplus = 0;
+  } else{
+    Letterplus = Hori_spacing*2/5;
+  }
 
   // Draw Rectangle
   context.fillStyle = "black"; // 사각형의 색상
@@ -42,7 +67,7 @@ function drawCanvas() {
 
   if (circleCount % 2 === 0){ //짝수
 
-    currentX = centerX + Hori_spacing/2;
+    currentX = centerX + Mid_spacing + Hori_spacing/2;
 
     for (let i = 1; i < RowCircleCount; i++) {
       for (let j = 0; j < circleCount/2; j++) {
@@ -55,7 +80,7 @@ function drawCanvas() {
         circles.push(currentX);
         currentX += Hori_spacing;
       }
-      currentX = centerX-Hori_spacing/2;
+      currentX = centerX - Mid_spacing -Hori_spacing/2;
 
       for (let k = 1; k <= circleCount/2; k++) {
         context.beginPath();
@@ -67,7 +92,7 @@ function drawCanvas() {
         circles.push(currentX);
         currentX -= Hori_spacing;
       }
-      currentX = centerX + Hori_spacing/2;
+      currentX = centerX + Mid_spacing + Hori_spacing/2;
       currentY -= Vert_spacing;
     }
 
@@ -83,6 +108,22 @@ function drawCanvas() {
     context.strokeStyle = "black";
     context.stroke();
     context.closePath();
+
+    //전체 폭 표시
+    context.beginPath();
+    context.moveTo(circles[rightCircleValue] , dataY+20); // 가로선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 가로선 끝점
+    context.moveTo(circles[LeftCircleValue] , dataY); // 왼쪽선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 왼쪽선 끝점
+    context.moveTo(circles[rightCircleValue] , dataY); // 오른쪽선 시작점
+    context.lineTo(circles[rightCircleValue] , dataY+20); // 오른쪽선 끝점
+    context.strokeStyle = "black";
+    context.stroke();
+    context.closePath();
+
+    context.font = "15px Arial";
+    context.fillStyle = "black";
+    context.fillText(xLength, centerX-10, dataY+35); // x폭 표시
 
 
     for (let l = 0; l < circleCount; l++) {
@@ -104,7 +145,7 @@ function drawCanvas() {
       const distanceRight = circles[o+1] - circles[o];
       context.font = "15px Arial";
       context.fillStyle = "black";
-      context.fillText(`${Math.abs(distanceRight)}`, circles[o], dataY+20);
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o]+Letterplus, dataY+15);
 
       } else if (o === circleCount/2-1) {
 
@@ -112,17 +153,17 @@ function drawCanvas() {
       const distanceRight = circles[o+1] - circles[0];
       context.font = "15px Arial";
       context.fillStyle = "black";
-      context.fillText(`${Math.abs(distanceRight)}`, circles[o+1], dataY+20);
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o+1]+Letterplus, dataY+15);
       } else {
 
       //왼쪽 원 사이에 거리 표시
       const distanceleft = circles[o+1] - circles[o];
       context.font = "15px Arial";
       context.fillStyle = "black";
-      context.fillText(`${Math.abs(distanceleft)}`, circles[o] + distanceleft, dataY+20);
+      context.fillText(`${Math.abs(distanceleft)}`, circles[o]+Letterplus + distanceleft, dataY+15);
       }
     }
-  } else{  //홀수
+  } else {  //홀수
     for (let i = 1; i < RowCircleCount; i++) {
       for (let j = 0; j < circleCount/2; j++) {
         context.beginPath();
@@ -134,7 +175,7 @@ function drawCanvas() {
         circles.push(currentX);
         currentX += Hori_spacing;
       }
-      currentX = centerX-Hori_spacing;
+      currentX = centerX - Mid_spacing -Hori_spacing;
 
       for (let k = 1; k < circleCount/2; k++) {
         context.beginPath();
@@ -146,7 +187,7 @@ function drawCanvas() {
         circles.push(currentX);
         currentX -= Hori_spacing;
       }
-      currentX = centerX;
+      currentX = centerX + Mid_spacing;
       currentY -= Vert_spacing;
     }
 
@@ -163,6 +204,21 @@ function drawCanvas() {
     context.stroke();
     context.closePath();
 
+    //전체 폭 표시
+    context.beginPath();
+    context.moveTo(circles[rightCircleValue] , dataY+20); // 가로선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 가로선 끝점
+    context.moveTo(circles[LeftCircleValue] , dataY); // 왼쪽선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 왼쪽선 끝점
+    context.moveTo(circles[rightCircleValue] , dataY); // 오른쪽선 시작점
+    context.lineTo(circles[rightCircleValue] , dataY+20); // 오른쪽선 끝점
+    context.strokeStyle = "black";
+    context.stroke();
+    context.closePath();
+
+    context.font = "15px Arial";
+    context.fillStyle = "black";
+    context.fillText(xLength, centerX-10, dataY+35); // x폭 표시
 
     for (let l = 0; l < circleCount; l++) {
       
@@ -174,82 +230,69 @@ function drawCanvas() {
       context.strokeStyle = "black";
       context.stroke();
       context.closePath();
-      }
+    }
     
-      for (let o = 0; o < circleCount-1 ; o++) {
-        if( o < (circleCount-1)/2) {
-  
-        //오른쪽 원 사이에 거리 표시
-        const distanceRight = circles[o+1] - circles[o];
-        context.font = "15px Arial";
-        context.fillStyle = "black";
-        context.fillText(`${Math.abs(distanceRight)}`, circles[o], dataY+20);
-  
-        } else if (o === (circleCount-1)/2) {
-  
-        //가운데 원 사이에 거리 표시
-        const distanceRight = circles[o+1] - circles[0];
-        context.font = "15px Arial";
-        context.fillStyle = "black";
-        context.fillText(`${Math.abs(distanceRight)}`, circles[o+1], dataY+20);
-        } else {
-  
-        //왼쪽 원 사이에 거리 표시
-        const distanceleft = circles[o+1] - circles[o];
-        context.font = "15px Arial";
-        context.fillStyle = "black";
-        context.fillText(`${Math.abs(distanceleft)}`, circles[o] + distanceleft, dataY+20);
-        }
+    for (let o = 0; o < circleCount-1 ; o++) {
+      if( o < (circleCount-1)/2) {
+
+      //오른쪽 원 사이에 거리 표시
+      const distanceRight = circles[o+1] - circles[o];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o]+Letterplus, dataY+15);
+
+      } else if (o === (circleCount-1)/2) {
+
+      //가운데 원 사이에 거리 표시
+      const distanceRight = circles[o+1] - circles[0];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o+1]+Letterplus, dataY+15);
+      } else {
+
+      //왼쪽 원 사이에 거리 표시
+      const distanceleft = circles[o+1] - circles[o];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceleft)}`, circles[o]+Letterplus + distanceleft, dataY+15);
       }
     }
-    //세로 거리 가로 선
-    context.beginPath();
-    context.moveTo(circles[LeftCircleValue]-diameter , FirstRowY-diameter);
-    context.lineTo(circles[LeftCircleValue] , FirstRowY-diameter);
-    context.strokeStyle = "black";
-    context.stroke();
-    context.closePath();
+  }
 
-    context.beginPath();
-    context.moveTo(circles[LeftCircleValue]-diameter , FirstRowY);
-    context.lineTo(circles[LeftCircleValue] , FirstRowY);
-    context.strokeStyle = "black";
-    context.stroke();
-    context.closePath();
 
-    //세로 거리 세로 선
-    context.beginPath();
-    context.moveTo(circles[LeftCircleValue]-diameter , FirstRowY);
-    context.lineTo(circles[LeftCircleValue]-diameter , FirstRowY-diameter);
-    context.strokeStyle = "black";
-    context.stroke();
-    context.closePath();
-    
+  //세로 거리 가로 선
+  context.beginPath();
+  context.moveTo(rectTopX - 20 , FirstRowY-Vert_spacing);
+  context.lineTo(circles[LeftCircleValue] , FirstRowY-Vert_spacing);
+  context.strokeStyle = "black";
+  context.stroke();
+  context.closePath();
 
-    //세로 거리 표기
-    context.font = "15px Arial";
-    context.fillStyle = "black";
-    context.fillText(Vert_spacing, circles[LeftCircleValue]-2*diameter , FirstRowY-diameter/2);
+  context.beginPath();
+  context.moveTo(rectTopX - 20 , FirstRowY);
+  context.lineTo(circles[LeftCircleValue] , FirstRowY);
+  context.strokeStyle = "black";
+  context.stroke();
+  context.closePath();
+
+  //세로 거리 세로 선
+  context.beginPath();
+  context.moveTo(rectTopX - 20 , FirstRowY);
+  context.lineTo(rectTopX - 20 , FirstRowY-Vert_spacing);
+  context.strokeStyle = "black";
+  context.stroke();
+  context.closePath();
   
 
+  //세로 거리 표기
+  context.font = "15px Arial";
+  context.fillStyle = "black";
+  context.fillText(Vert_spacing, rectTopX - 40 , FirstRowY-Vert_spacing*1/3);
 
-  /*// 사각형 가운데에 수평 중심선 그리기
-  context.beginPath();
-  context.moveTo(centerX, rectY); // 시작점
-  context.lineTo(centerX, rectY + xLength * 2.5); // 끝점
-  context.strokeStyle = "black"; // 선의 색상
-  context.stroke();
-  context.closePath();
-  */
-  /*// 사각형 가운데에 수평 중심선 그리기
-  context.beginPath();
-  context.moveTo(circlesLeft[1], FirstRowY); // 시작점
-  context.lineTo(circlesLeft[3], FirstRowY); // 끝점
-  context.strokeStyle = "black"; // 선의 색상
-  context.stroke();
-  context.closePath();
-  */
-
-
+  //마지막에 데이터 출력
+  var userInputText = `폭의 길이: ${xLength} cm 
+  가로 간격: ${Hori_spacing}cm, 세로 간격: ${Vert_spacing}cm
+  개수: ${circleCount}개, 지름: ${diameter}cm`;
+  document.getElementById("userInputText").innerText = userInputText;
   
 }
--- scriptcopy.js
+++ scriptcopy.js
@@ -1,3 +1,14 @@
+function toggleMid_spacingInput() {
+  var Mid_spacingInput = document.getElementById("Mid_spacingInput");
+  var Mid_spacingCheckbox = document.getElementById("Mid_spacingCheckbox");
+  
+  if (Mid_spacingCheckbox.checked) {
+    Mid_spacingInput.style.display = "block";
+  } else {
+    Mid_spacingInput.style.display = "none";
+  }
+}
+
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
@@ -7,6 +18,16 @@ function drawCanvas() {
   var diameter = parseInt(document.getElementById("diameterInput").value);
   var Hori_spacing = parseInt(document.getElementById("Hori_spacingInput").value);
   var Vert_spacing = parseInt(document.getElementById("Vert_spacingInput").value);
+
+  var Mid_spacingInput = document.getElementById("Mid_spacingInput"); //가운데 다를때
+
+  Mid_spacingInput.addEventListener("input", function() {
+    var Mid_spacingInputValue = Mid_spacingInput.value;
+    if (Mid_spacingInputValue !== "") {
+      // 동작을 수행하는 코드
+      Mid_spacingValue = Mid_spacingInputValue/2;
+    }
+  });
 
   context.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
 
@@ -18,6 +39,8 @@ function drawCanvas() {
   var rectBotY = canvas.height-70;
   var rectlenX = xLength;
   var rectlenY = xLength * 2.5;
+
+  var Letterplus = Hori_spacing/2;
   
   var currentX = centerX;
   var currentY = rectBotY - diameter;
@@ -25,25 +48,25 @@ function drawCanvas() {
   var RowCircleCount = xLength * 2.5 / Vert_spacing;
 
   var FirstRowY = rectBotY - diameter;
-  var dataY = rectBotY + diameter;
-  var circlesLeft = [];
-  var circlesRight = [];
+  var dataY = rectBotY + 10;
+  var circles = [];
 
-  console.log(rectTopX,rectTopY,rectBotX,rectBotY);
+  //console.log(rectTopX,rectTopY,rectBotX,rectBotY);
 
-  context.beginPath();
-  context.arc(400, 700, 10, 0, 2 * Math.PI); // 점을 그리는 부분
-  context.fillStyle = "blue"; // 점의 색상
-  context.fill();
-  context.closePath();
+  if(Hori_spacing < 30){
+    Letterplus = 0;
+  } else{
+    Letterplus = Hori_spacing*2/5;
+  }
 
   // Draw Rectangle
   context.fillStyle = "black"; // 사각형의 색상
   context.strokeRect(rectTopX, rectTopY, rectlenX, rectlenY);
+  
 
-  if (circleCount % 2 === 0){
+  if (circleCount % 2 === 0){ //짝수
 
-    currentX = centerX + Hori_spacing/2;
+    currentX = centerX + Mid_spacingValue + Hori_spacing/2;
 
     for (let i = 1; i < RowCircleCount; i++) {
       for (let j = 0; j < circleCount/2; j++) {
@@ -53,10 +76,10 @@ function drawCanvas() {
         context.stroke();
         context.closePath();
         
-        circlesRight.push(currentX);
+        circles.push(currentX);
         currentX += Hori_spacing;
       }
-      currentX = centerX-Hori_spacing/2;
+      currentX = centerX + Mid_spacingValue -Hori_spacing/2;
 
       for (let k = 1; k <= circleCount/2; k++) {
         context.beginPath();
@@ -65,13 +88,81 @@ function drawCanvas() {
         context.stroke();
         context.closePath();
         
-        circlesLeft.push(currentX);
+        circles.push(currentX);
         currentX -= Hori_spacing;
       }
-      currentX = centerX + Hori_spacing/2;
+      currentX = centerX + Mid_spacingValue + Hori_spacing/2;
       currentY -= Vert_spacing;
     }
-  } else{
+
+    // 길이 표기
+
+    rightCircleValue = circleCount/2-1;
+    LeftCircleValue = circleCount-1;
+  
+    //좌우 길이 표시
+    context.beginPath();
+    context.moveTo(circles[rightCircleValue] , dataY);
+    context.lineTo(circles[LeftCircleValue] , dataY);
+    context.strokeStyle = "black";
+    context.stroke();
+    context.closePath();
+
+    //전체 폭 표시
+    context.beginPath();
+    context.moveTo(circles[rightCircleValue] , dataY+20); // 가로선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 가로선 끝점
+    context.moveTo(circles[LeftCircleValue] , dataY); // 왼쪽선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 왼쪽선 끝점
+    context.moveTo(circles[rightCircleValue] , dataY); // 오른쪽선 시작점
+    context.lineTo(circles[rightCircleValue] , dataY+20); // 오른쪽선 끝점
+    context.strokeStyle = "black";
+    context.stroke();
+    context.closePath();
+
+    context.font = "15px Arial";
+    context.fillStyle = "black";
+    context.fillText(xLength, centerX-10, dataY+35); // x폭 표시
+
+
+    for (let l = 0; l < circleCount; l++) {
+      
+      //위아래 길이 표시
+  
+      context.beginPath();
+      context.moveTo(circles[l] , FirstRowY);
+      context.lineTo(circles[l] , dataY);
+      context.strokeStyle = "black";
+      context.stroke();
+      context.closePath();
+      }
+    
+    for (let o = 0; o < circleCount-1 ; o++) {
+      if( o < circleCount/2-1) {
+
+      //오른쪽 원 사이에 거리 표시
+      const distanceRight = circles[o+1] - circles[o];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o]+Letterplus, dataY+15);
+
+      } else if (o === circleCount/2-1) {
+
+      //가운데 원 사이에 거리 표시
+      const distanceRight = circles[o+1] - circles[0];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o+1]+Letterplus, dataY+15);
+      } else {
+
+      //왼쪽 원 사이에 거리 표시
+      const distanceleft = circles[o+1] - circles[o];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceleft)}`, circles[o]+Letterplus + distanceleft, dataY+15);
+      }
+    }
+  } else {  //홀수
     for (let i = 1; i < RowCircleCount; i++) {
       for (let j = 0; j < circleCount/2; j++) {
         context.beginPath();
@@ -80,10 +171,10 @@ function drawCanvas() {
         context.stroke();
         context.closePath();
         
-        circlesRight.push(currentX);
+        circles.push(currentX);
         currentX += Hori_spacing;
       }
-      currentX = centerX-Hori_spacing;
+      currentX = centerX - Mid_spacingValue - Hori_spacing;
 
       for (let k = 1; k < circleCount/2; k++) {
         context.beginPath();
@@ -92,83 +183,115 @@ function drawCanvas() {
         context.stroke();
         context.closePath();
         
-        circlesLeft.push(currentX);
+        circles.push(currentX);
         currentX -= Hori_spacing;
       }
-      currentX = centerX;
+      currentX = centerX + Mid_spacingValue;
       currentY -= Vert_spacing;
+    }
+
+    //길이 표기 
+
+    rightCircleValue = (circleCount-1)/2;
+    LeftCircleValue = circleCount-1;
+  
+    //좌우 길이 선
+    context.beginPath();
+    context.moveTo(circles[rightCircleValue] , dataY);
+    context.lineTo(circles[LeftCircleValue] , dataY);
+    context.strokeStyle = "black";
+    context.stroke();
+    context.closePath();
+
+    //전체 폭 표시
+    context.beginPath();
+    context.moveTo(circles[rightCircleValue] , dataY+20); // 가로선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 가로선 끝점
+    context.moveTo(circles[LeftCircleValue] , dataY); // 왼쪽선 시작점
+    context.lineTo(circles[LeftCircleValue] , dataY+20); // 왼쪽선 끝점
+    context.moveTo(circles[rightCircleValue] , dataY); // 오른쪽선 시작점
+    context.lineTo(circles[rightCircleValue] , dataY+20); // 오른쪽선 끝점
+    context.strokeStyle = "black";
+    context.stroke();
+    context.closePath();
+
+    context.font = "15px Arial";
+    context.fillStyle = "black";
+    context.fillText(xLength, centerX-10, dataY+35); // x폭 표시
+
+    for (let l = 0; l < circleCount; l++) {
+      
+      //위아래 길이 표시
+  
+      context.beginPath();
+      context.moveTo(circles[l] , FirstRowY);
+      context.lineTo(circles[l] , dataY);
+      context.strokeStyle = "black";
+      context.stroke();
+      context.closePath();
+    }
+    
+    for (let o = 0; o < circleCount-1 ; o++) {
+      if( o < (circleCount-1)/2) {
+
+      //오른쪽 원 사이에 거리 표시
+      const distanceRight = circles[o+1] - circles[o];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o]+Letterplus, dataY+15);
+
+      } else if (o === (circleCount-1)/2) {
+
+      //가운데 원 사이에 거리 표시
+      const distanceRight = circles[o+1] - circles[0];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceRight)}`, circles[o+1]+Letterplus, dataY+15);
+      } else {
+
+      //왼쪽 원 사이에 거리 표시
+      const distanceleft = circles[o+1] - circles[o];
+      context.font = "15px Arial";
+      context.fillStyle = "black";
+      context.fillText(`${Math.abs(distanceleft)}`, circles[o]+Letterplus + distanceleft, dataY+15);
+      }
     }
   }
 
-  /*// 사각형 가운데에 수평 중심선 그리기
+
+  //세로 거리 가로 선
   context.beginPath();
-  context.moveTo(centerX, rectY); // 시작점
-  context.lineTo(centerX, rectY + xLength * 2.5); // 끝점
-  context.strokeStyle = "black"; // 선의 색상
+  context.moveTo(rectTopX - 20 , FirstRowY-Vert_spacing);
+  context.lineTo(circles[LeftCircleValue] , FirstRowY-Vert_spacing);
+  context.strokeStyle = "black";
   context.stroke();
   context.closePath();
-  */
-  /*// 사각형 가운데에 수평 중심선 그리기
+
   context.beginPath();
-  context.moveTo(circlesLeft[1], FirstRowY); // 시작점
-  context.lineTo(circlesLeft[3], FirstRowY); // 끝점
-  context.strokeStyle = "black"; // 선의 색상
+  context.moveTo(rectTopX - 20 , FirstRowY);
+  context.lineTo(circles[LeftCircleValue] , FirstRowY);
+  context.strokeStyle = "black";
   context.stroke();
   context.closePath();
-  */
 
-  console.log(circlesLeft);
-  console.log(circleCount);
-  console.log(FirstRowY);
-  console.log(circlesLeft[0]);
-  console.log(circleCount/2 - 1);
+  //세로 거리 세로 선
+  context.beginPath();
+  context.moveTo(rectTopX - 20 , FirstRowY);
+  context.lineTo(rectTopX - 20 , FirstRowY-Vert_spacing);
+  context.strokeStyle = "black";
+  context.stroke();
+  context.closePath();
+  
 
-  for (let l = 0; l < circleCount/2; l++) {
-      //왼쪽 좌우 길이 표시
-      context.beginPath();
-      context.moveTo(circlesLeft[l] , dataY);
-      context.lineTo(circlesLeft[l + 1], dataY);
-      context.strokeStyle = "black";
-      context.stroke();
-      context.closePath();
+  //세로 거리 표기
+  context.font = "15px Arial";
+  context.fillStyle = "black";
+  context.fillText(Vert_spacing, rectTopX - 40 , FirstRowY-Vert_spacing*1/3);
 
-      //왼쪽 위아래 길이 표시
-
-      context.beginPath();
-      context.moveTo(circlesLeft[l+1] , FirstRowY);
-      context.lineTo(circlesLeft[l+1], dataY);
-      context.strokeStyle = "black";
-      context.stroke();
-      context.closePath();
-
-      //오른쪽 좌우 길이 표시
-      context.beginPath();
-      context.moveTo(circlesRight[l] , dataY);
-      context.lineTo(circlesRight[l + 1], dataY);
-      context.strokeStyle = "black";
-      context.stroke();
-      context.closePath();
-
-      //오른쪽 위아래 길이 표시
-
-      context.beginPath();
-      context.moveTo(circlesRight[l+1] , FirstRowY);
-      context.lineTo(circlesRight[l+1], dataY);
-      context.strokeStyle = "black";
-      context.stroke();
-      context.closePath();
-  }
-  for (let o = 0; o < circleCount/2-1; o++) {
-    //왼쪽 원 사이에 거리 표시
-    const distanceleft = circlesLeft[o+1] - circlesLeft[o];
-    context.font = "15px Arial";
-    context.fillStyle = "black";
-    context.fillText(`${Math.abs(distanceleft)}`, circlesLeft[o] + distanceleft, dataY);
-
-    //오른쪽 원 사이에 거리 표시
-    const distanceRight = circlesRight[o+1] - circlesRight[o];
-    context.font = "15px Arial";
-    context.fillStyle = "black";
-    context.fillText(`${Math.abs(distanceRight)}`, circlesRight[o] + distanceRight, dataY);
-  }
+  //마지막에 데이터 출력
+  var userInputText = `폭의 길이: ${xLength} cm 
+  가로 간격: ${Hori_spacing}cm, 세로 간격: ${Vert_spacing}cm
+  개수: ${circleCount}개, 지름: ${diameter}cm`;
+  document.getElementById("userInputText").innerText = userInputText;
+  
 }
